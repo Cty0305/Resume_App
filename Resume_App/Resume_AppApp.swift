1531cf6c9232c4a8824adaa1b6f50f984a29aa24
alanchou0305 <mickeychou375@gmail.com>
2023-09-12 11:31:31 +0800

    Initial Commit

---
+++ Resume_App/Resume_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_AppApp.swift
+//  Resume_App
+//
+//  Created by hububble on 2023/9/12.
+//
+
+import SwiftUI
+
+@main
+struct Resume_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
